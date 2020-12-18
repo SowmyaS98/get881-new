be8c38b78237ee5b718b9c92e0e4f6549f133b6f
SowmyaS98 <76148904+SowmyaS98@users.noreply.github.com>
2020-12-18 16:48:53 +0530

    added product method

--- demoapp/src/com/lti/model/Calc.java
+++ demoapp/src/com/lti/model/Calc.java
@@ -7,6 +7,11 @@ public class Calc {
 		System.out.println("division result is: "+(n1/n2));
 
 	}
+	public void mul(int n1,int n2)
+	{
+		System.out.println("mul result is: "+(n1*n2));
+
+	}
 	
 	
 }

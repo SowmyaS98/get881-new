c791c360fb143141a2a12eea8de00d66db0cb481
SowmyaS98 <sowmyselvaraj.ssk@gmail.com>
2020-12-18 16:41:54 +0530

    added subs method in new branch

--- demoapp/src/com/lti/model/Calc.java
+++ demoapp/src/com/lti/model/Calc.java
@@ -4,8 +4,13 @@ public class Calc {
 	
 	public void div(int n1,int n2)
 	{
-		System.out.println("division result is: "+(n1*n2));
+		System.out.println("division result is: "+(n1/n2));
 	}
+	public void subs(int n1,int n2)
+	{
+		System.out.println("difference result is: "+(n1-n2));
+	}
+	
 	
 	
 }

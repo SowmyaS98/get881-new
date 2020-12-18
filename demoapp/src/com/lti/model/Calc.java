22e1c5ff41ea892db400001f984cd4061e4313b4
SowmyaS98 <sowmyselvaraj.ssk@gmail.com>
2020-12-18 16:06:55 +0530

    first commit-project created

---
+++ demoapp/src/com/lti/model/Calc.java
@@ -0,0 +1,11 @@
+package com.lti.model;
+
+public class Calc {
+	
+	public void div(int n1,int n2)
+	{
+		System.out.println("div is: "+(n1/n2));
+	}
+	
+	
+}
---
+++ demoapp/src/com/lti/model/area.java
@@ -0,0 +1,9 @@
+package com.lti.model;
+
+public class area {
+	public void findarea()
+	{
+		System.out.println("hi");
+	}
+
+}
